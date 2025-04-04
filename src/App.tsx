f5cfaa5b46fb61e7f8225f68a9d1753982f9e978
doabledanny <dantheado@gmail.com>
2025-04-05 00:00:35 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import type {
+  User,
+  ChannelSort,
+  ChannelFilters,
+  ChannelOptions,
+} from "stream-chat";
+import {
+  useCreateChatClient,
+  Chat,
+  Channel,
+  ChannelList,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+import "stream-chat-react/dist/css/v2/index.css";
+import MyChannelHeader from "./MyChannelHeader";
+import MyAIStateIndicator from "./MyAIStateIndicator";
+import MyMessage from "./MyMessage";
+
+// your Stream app information
+const apiKey = import.meta.env.VITE_STREAM_API_KEY;
+const userToken = import.meta.env.VITE_STREAM_TOKEN;
+const userId = "react-app";
+const userName = "React App";
+
+if (!apiKey || !userToken) {
+  throw new Error("Missing API key or user token");
+}
+
+const user: User = {
+  id: userId,
+  name: userName,
+  image:
+    "https://vignette.wikia.nocookie.net/starwars/images/6/6f/Anakin_Skywalker_RotS.png",
+};
+
+const sort: ChannelSort = { last_message_at: -1 };
+const filters: ChannelFilters = {
+  type: "messaging",
+  members: { $in: [userId] },
+};
+const options: ChannelOptions = {
+  limit: 10,
+};
+
+const App = () => {
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
+  return (
+    <Chat client={client}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel>
+        <Window>
+          <MyChannelHeader />
+          <MessageList />
+          <MyAIStateIndicator />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default App;
